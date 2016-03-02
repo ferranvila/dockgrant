169e022839240529a99541cbf2d89ed170c8b3fd
Vila Conesa, Ferran <Ferran.Vila@gft.com>
2016-03-02 18:15:47 +0100

    Vagrantfile templating

--- src/dockgrant-run.js
+++ src/dockgrant-run.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var program = require('commander');
-var common = require('../lib/common');
 
 program
+    .option('-v, --volume host:guest', 'Share a host folder into the guest', null, null)
+    .option('-w, --workdir [workdir]', 'Working directory inside the virtual image', null, null)
+    .option('-d, --debug', 'Enable verbose mode', null, null)
     .parse(process.argv);
 
-common.log('run');
+require('./impl/run').run(program, function () {
+
+});
